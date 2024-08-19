cd9a2261b13959f754f010dee3d22ee7d522bd94
barbaraakk <barbaram2627@gmail.com>
2024-08-19 17:27:06 -0300

    ultimo teste de atualização

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Carregar variáveis de ambiente
 const twitterBearerToken = process.env.TWITTER_BEARER_TOKEN;
