97bc71866b6db6c1c61ca14b9a7e7dc87265f9d7
barbaraakk <barbaram2627@gmail.com>
2024-08-21 02:33:53 -0300

    desistencia de adição da api do twitter por que é pago

--- server/index.js
+++ server/index.js
@@ -1,37 +1,12 @@
 const express = require('express');
-const axios = require('axios');
 require('dotenv').config(); // Carregar variáveis de ambiente
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
-
-
-// Carregar variáveis de ambiente
-const twitterBearerToken = process.env.TWITTER_BEARER_TOKEN;
 
 // Serve arquivos estáticos da pasta 'public'
 app.use(express.static('public'));
-
-// Rota para buscar tweets
-app.get('/tweets', async (req, res) => {
-  try {
-    const response = await axios.get('https://api.twitter.com/2/tweets/search/recent', {
-      headers: {
-        'Authorization': `Bearer ${twitterBearerToken}`, // Usando a variável carregada
-        'User-Agent': 'TweetFetcher/1.0' // Nome e versão da sua aplicação
-      },
-      params: {
-        query: 'from:MandZatsu', // Ajuste conforme necessário
-        max_results: 5 // Número máximo de tweets retornados
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error('Erro ao buscar tweets:', error.response ? error.response.data : error.message);
-    res.status(500).send('Erro ao buscar tweets');
-  }
-});
 
 // Rota para servir a página inicial
 app.get('/', (req, res) => {
